be373d49e768f059f4bb53847c771797297a9905
Tyuiu.SamarAA@outlook.com <istore103@icloud.com>
2023-10-28 17:15:32 +0500

    Task4

---
+++ Tyuiu.SamarAA.Sprint3.Task4.V11.Lib/DataService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.SamarAA.Sprint3.Task4.V11.Lib
+{
+    public class DataService : ISprint3Task4V11
+    {
+        public double Calculate(int startValue, int stopValue)
+        {
+            double res = 1;
+            for(int x = -5; x<=5; x++)
+            {
+                if (x == 0)
+                {
+                    continue;
+                }
+                res = res * (x / (Math.Sin(x) - x)) + 2;
+            }
+            return Math.Round(res, 3);
+        }
+    }
+}
---
+++ Tyuiu.SamarAA.Sprint3.Task4.V11.Test/DataServiceTest.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.SamarAA.Sprint3.Task4.V11.Lib;
+
+namespace Tyuiu.SamarAA.Sprint3.Task4.V11.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+
+            int startValue = -5;
+            int stopValue = 5;
+
+            double res = ds.Calculate(startValue, stopValue);
+            double wait = 2.186;
+
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
